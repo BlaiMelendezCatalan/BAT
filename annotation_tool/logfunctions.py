99583e02db0c6b5a60bdad2f825a713464bb96c4
Blai Melendez Catalan <bmelendez@bmat.com>
2017-02-23 17:07:54 +0100

    initial branch commit

---
+++ annotation_tool/logfunctions.py
@@ -0,0 +1,8 @@
+import os
+from math import ceil, floor
+import numpy as np
+import random
+import datetime
+from annotation_tool.models import Project, Wav, Segment, Annotation, Event, Tag, Class, ClassProminence, Annotator
+import annotation_tool.utils
+
