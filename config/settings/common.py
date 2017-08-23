851f9806620a8b5149379be76514b9e54f0f9eb8
Blai Melendez Catalan <bmelendez@bmat.com>
2017-08-23 19:56:57 +0200

    docker clean up

--- config/settings/common.py
+++ config/settings/common.py
@@ -81,7 +81,7 @@ EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND', default='django.core.mail.backends.s
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#admins
 ADMINS = (
-    ("""ITBear""", 'pugach.itbear@gmail.com'),
+    ("""BlaiMelendezCatalan""", 'blaimelcat@gmail.com'),
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#managers
